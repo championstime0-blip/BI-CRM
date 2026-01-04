569aa27b53ed7a6f2aef75c181cd9eba04bf7cfd
championstime0-blip <championstime0@gmail.com>
2026-01-04 04:11:46 -0300

    Update metrics.py

--- backend/metrics.py
+++ backend/metrics.py
@@ -1,1 +1,8 @@
+def kpis_gerais(df):
+    return {
+        "total": len(df),
+        "andamento": len(df[df["Status"]=="Em Andamento"]),
+        "perdidos": len(df[df["Status"]=="Perdido"]),
+        "ganhos": len(df[df["Status"]=="Ganho"])
+    }
 
