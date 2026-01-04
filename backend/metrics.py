5ca03c47223a7d11aac4f9e04030040ee2794136
championstime0-blip <championstime0@gmail.com>
2026-01-04 17:37:49 -0300

    Delete backend directory

--- backend/metrics.py
+++
@@ -1,8 +0,0 @@
-def kpis_gerais(df):
-    return {
-        "total": len(df),
-        "andamento": len(df[df["Status"]=="Em Andamento"]),
-        "perdidos": len(df[df["Status"]=="Perdido"]),
-        "ganhos": len(df[df["Status"]=="Ganho"])
-    }
-
